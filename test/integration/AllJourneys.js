493b1238954e9b3f8a9a1f0d5c39ea268d0e434e
S0021493557 <sudipta.raut@wipro.com>
2020-04-09 14:17:59 +0000

    Import content for version 1.0.5

---
+++ test/integration/AllJourneys.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/test/Opa5","./arrangements/Startup","./NavigationJourney"],function(e,a){"use strict";e.extendConfig({arrangements:new a,viewNamespace:"com.qb2i4.qb2i4.view.",autoWait:true})});
